3ac011ebac51c223e639e3ac506972bdbd7f92f3
Farid Zainulin <10221278@mail.ru>
2024-01-11 20:23:17 +0300

    Добавил страницу товара

--- imagazine/goods/urls.py
+++ imagazine/goods/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from imagazine.goods.views import *
 
 urlpatterns = [ 
-               path('', GamesIndexView.as_view(), name='index')]
+               path('', GamesIndexView.as_view(), name='index'),
+               path('games/<int:pk>/', ProfileGameView.as_view(), name='profile')]
--- imagazine/goods/views.py
+++ imagazine/goods/views.py
@@ -2,7 +2,7 @@ from typing import Any
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Game, Console
 
 # Create your views here.
@@ -18,3 +18,14 @@ class GamesIndexView(ListView):
     
     def get_queryset(self):
         return Game.objects.all().order_by('-count')
+    
+class ProfileGameView(DetailView):
+    model = Game
+    template_name = 'goods/profile.html'
+    context_object_name = 'game'
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        content = super().get_context_data(**kwargs)
+        content['title'] = 'Игра' + content['game'].name
+        content['cat'] = '/'.join([name.name for name in content['game'].category.all()])
+        return content
